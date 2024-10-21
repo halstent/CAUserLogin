cdd895ac75c80686fee364be488684b40ea88435
Halsten <h.tilk@mail.utoronto.ca>
2024-10-21 10:27:01 -0400

    Task 2.1

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,9 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Sets the current user with the given identifier.
+     * @param name the name to set
+     */
+    void setCurrentUser(String name);
 }
